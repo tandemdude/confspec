f799583dc925da68920e83174d58d3f88ccecdd6
tandemdude <43570299+tandemdude@users.noreply.github.com>
2026-07-13 09:52:03 +0100

    chore: ruff

--- tests/test_path.py
+++ tests/test_path.py
@@ -3,14 +3,17 @@ import pytest
 from confspec import path
 
 
-@pytest.mark.parametrize("expr,expected", [
-    ("foo", ("foo",)),
-    ("foo.bar", ("foo", "bar")),
-    ("foo[0]", ("foo", 0)),
-    ("foo.bar[0]", ("foo", "bar", 0)),
-    ("foo[0][0]", ("foo", 0, 0)),
-    ("foo[0].bar[0].baz", ("foo", 0, "bar", 0, "baz"))
-])
+@pytest.mark.parametrize(
+    "expr,expected",
+    [
+        ("foo", ("foo",)),
+        ("foo.bar", ("foo", "bar")),
+        ("foo[0]", ("foo", 0)),
+        ("foo.bar[0]", ("foo", "bar", 0)),
+        ("foo[0][0]", ("foo", 0, 0)),
+        ("foo[0].bar[0].baz", ("foo", 0, "bar", 0, "baz")),
+    ],
+)
 def test_parse_path(expr: str, expected: path.PathT) -> None:
     assert path.parse_path(expr) == expected
 
